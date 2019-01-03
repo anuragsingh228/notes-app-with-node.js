a7d3275bd56cbd8be63d168abb3e09b6c18bc8ea
Anurag Singh <anurags228@gmail.com>
2019-01-03 13:11:34 +0530

    notes app in node

---
+++ notes.js
@@ -0,0 +1,73 @@
+
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    }
+
+    var duplicatesNotes = notes.filter((note) => {
+        return note.title == title;
+    })
+    if (duplicatesNotes.length == 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+
+};
+
+var getAll = () => {
+    return fetchNotes();
+};
+
+var getNote = (title) => {
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => {
+        return note.title == title;
+    });
+    return filteredNotes[0];
+};
+
+var removeNote = (title) => {
+    //fetch note
+    var notes = fetchNotes();
+    //filter notes, removing the one with title of arguement
+    var filteredNotes = notes.filter((note) => {
+        return note.title != title;
+    });
+    //save new notes array
+    saveNotes(filteredNotes);
+
+    return notes.length != filteredNotes.length;
+};
+
+var logNote = (note) => {
+
+    console.log('----');
+    console.log('Title: ' + note.title);
+    console.log('Body: ' + note.body);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote,
+    logNote
+};
---
+++ playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'Anurag'
+};
+
+person.age = 25;
+
+person.name = 'mike';
+
+console.log(person);
